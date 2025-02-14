59e5150d5eadee9f8a44ce50eb325bb6c8312bfa
Rohit Lakhera <rohitlakhera2682@gmail.com>
2025-02-14 22:42:20 +0530

    added pollyfill.ts

---
+++ polyfill.ts
@@ -0,0 +1,21 @@
+// polyfill.ts
+interface PromiseWithResolvers<T> {
+    promise: Promise<T>;
+    resolve: (value: T | PromiseLike<T>) => void;
+    reject: (reason?: any) => void;
+  }
+  
+  if (typeof Promise.withResolvers !== 'function') {
+    Promise.withResolvers = function <T>(): PromiseWithResolvers<T> {
+      let resolve!: (value: T | PromiseLike<T>) => void;
+      let reject!: (reason?: any) => void;
+  
+      const promise = new Promise<T>((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+  
+      return { promise, resolve, reject };
+    };
+  }
+  
