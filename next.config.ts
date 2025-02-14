45966304d4bc1debb2b106e836a46d80acbd07a7
Rohit Lakhera <rohitlakhera2682@gmail.com>
2025-02-14 21:13:08 +0530

    init23

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
+  assetPrefix: isProd ? '/RohitLakhera2682.github.io' : '',
+  basePath: isProd ? '/RohitLakhera2682.github.io' : '',
   output: 'export'
 };
 
